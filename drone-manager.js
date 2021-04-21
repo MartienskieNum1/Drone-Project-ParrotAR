1d0117d9e8d6dfb0a1e6cd41196a06dcf79de5f4
Kwinten Tas <kwinten.tas@student.howest.be>
2021-04-21 13:11:59 +0200

    temp commit

--- drone-manager.js
+++ drone-manager.js
@@ -4,16 +4,37 @@ const autonomy = require('ardrone-autonomy');
 function create() {
 
 
-    function parseSequence() {
-        console.log("Parsing the sequence...");
+    function parseSequence(msg) {
+        let mission  = autonomy.createMission();
+
+        let actions = JSON.parse(msg);
+
+        for(const action of actions){
+            appendMission(mission, a.action, a.param);
+        }
+
+        return mission;
     }
 
-    function execute() {
+    function execute(msg) {
+        const mission = parseSequence(msg);
+        console.log(mission);
         console.log("Executing mission...");
     }
 
     function abort() {
         console.log("Aborting mission...");
+    }
+
+    function appendMission(mission, action, param){
+        switch(action){
+            case "Forward":
+                mission.forward(param);
+                break;
+            case "Backward":
+                mission.backward(param);
+                break;
+        }
     }
 
     return { parseSequence, execute, abort };
