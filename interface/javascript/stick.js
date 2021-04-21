6fd233811219ecb4cda9af86b13dbdc34c603bb3
Maarten De Meyere <maarten.de.meyere@student.howest.be>
2021-04-21 22:52:51 +0200

    add second stick + fix turn left

--- interface/javascript/stick.js
+++ interface/javascript/stick.js
@@ -14,6 +14,7 @@ let init = () => {
 };
 document.addEventListener('DOMContentLoaded', init);
 
+// keyboard input
 let navigate = (e) => {
     keysPara.innerHTML += e.key;
 
@@ -44,23 +45,51 @@ let navigateUp = (e) => {
     }
 };
 
-// Create JoyStick object into the DIV 'joy1Div'
+// joystick input
+// left joystick
 var Joy1 = new JoyStick('joy1Div');
 
-var joy1IinputPosX = document.getElementById("joy1PosizioneX");
-var joy1InputPosY = document.getElementById("joy1PosizioneY");
-var joy1Direzione = document.getElementById("joy1Direzione");
 var joy1X = document.getElementById("joy1X");
 var joy1Y = document.getElementById("joy1Y");
 
-setInterval(function () { joy1IinputPosX.value=Joy1.GetPosX(); }, 50);
-setInterval(function () { joy1InputPosY.value=Joy1.GetPosY(); }, 50);
-setInterval(function () { joy1Direzione.value=Joy1.GetDir(); }, 50);
 setInterval(function () { joy1X.value=Joy1.GetX(); }, 50);
 setInterval(function () { joy1Y.value=Joy1.GetY(); }, 50);
 setInterval(function () {
     let x = Joy1.GetX();
     let y = Joy1.GetY();
+    let data = new Object();
+
+    if (x != 0 && y != 0) {
+        if (y > 0 && y > Math.abs(x)) {
+            data.state = "LeftStickUp";
+            data.speed = y / 100;
+        } else if (y < 0 && Math.abs(y) > Math.abs(x)) {
+            data.state = "LeftStickDown";
+            data.speed = y / 100 * -1;
+        } else if (x > 0 && x > Math.abs(y)) {
+            data.state = "LeftStickRight";
+            data.speed = x / 100;
+        } else if (x < 0 && Math.abs(x) > Math.abs(y)) {
+            data.state = "LeftStickLeft";
+            data.speed = x / 100 * -1;
+        }
+    
+        emit("executeStick", JSON.stringify(data));   
+    }
+
+}, 1000);
+
+// right joystick
+var Joy2 = new JoyStick('joy2Div');
+
+var joy2X = document.getElementById("joy2X");
+var joy2Y = document.getElementById("joy2Y");
+
+setInterval(function () { joy2X.value=Joy2.GetX(); }, 50);
+setInterval(function () { joy2Y.value=Joy2.GetY(); }, 50);
+setInterval(function () {
+    let x = Joy2.GetX();
+    let y = Joy2.GetY();
     let data = new Object();
 
     if (y > 0 && y > Math.abs(x)) {
@@ -72,7 +101,7 @@ setInterval(function () {
     } else if (x > 0 && x > Math.abs(y)) {
         data.state = "RightStickRight";
         data.speed = x / 100;
-    } else if (x < 0 && Math.abs(x) < Math.abs(y)) {
+    } else if (x < 0 && Math.abs(x) > Math.abs(y)) {
         data.state = "RightStickLeft";
         data.speed = x / 100 * -1;
     } else {
@@ -82,7 +111,7 @@ setInterval(function () {
 
     emit("executeStick", JSON.stringify(data));
 
-}, 50);
+}, 1000);
 
 let emit = (command, payload) => {
     socket.emit(command, payload);
