62e8378a96807389356f9fed259498f95bee5ea1
marijn.abelshausen <marijn.abelshausen@student.howest.be>
2021-04-22 11:19:28 +0200

    Edit drone-test

--- drone-test.js
+++ drone-test.js
@@ -2,3 +2,6 @@ const arDrone = require('ar-drone');
 const client = arDrone.createClient();
 
 client.config('general:navdata_demo', 'FALSE');
+client.on('navdata', (data)=>{
+    console.log(data['dronestate']);
+});
