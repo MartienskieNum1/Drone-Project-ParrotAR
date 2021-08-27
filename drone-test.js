f1b8a3424c4087fa841811c17cdc4b76039309c0
Maarten De Meyere <maarten.de.meyere@student.howest.be>
2021-08-28 00:04:25 +0200

    delete unnecessary files

--- drone-test.js
+++
@@ -1,8 +0,0 @@
-const arDrone = require('ar-drone');
-const client = arDrone.createClient();
-
-client.config('general:navdata_demo', 'FALSE');
-client.on('navdata', (data)=>{
-    console.log(data['dronestate']);
-});
-client.createRepl();
